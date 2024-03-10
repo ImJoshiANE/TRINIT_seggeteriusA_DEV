b2d3775f60014c4755bdfee734041cf03edfb3e7
Dheeraj <dkjoshi8293@gmail.com>
2024-03-10 06:06:28 +0530

    Home page dynamic

--- Frontend/src/components/tutorCard/TutorCard.jsx
+++ Frontend/src/components/tutorCard/TutorCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Card,
   CardContent,
@@ -6,24 +6,30 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Badge } from "@/components/ui/badge"
-import { Button } from '../ui/button';
-import {useNavigate} from "react-router-dom";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "../ui/button";
+import { useNavigate } from "react-router-dom";
 
-const TutorCard = ({tutor}) => {
+const TutorCard = ({ tutor, i }) => {
   const navigate = useNavigate();
-  const avgPrice = tutor.pricing.reduce((acc, it) => acc + it.price, 0) / tutor.pricing.length;
+  const avgPrice = Math.floor(
+    (tutor.pricing[0]["beginner"] +
+      tutor.pricing[0]["intermediate"] +
+      tutor.pricing[0]["advance"]) /
+      3
+  );
+  // console.log(tutor);
   return (
     <Card className="h-50 w-60">
       <CardHeader className="flex content-between">
         <div className="flex justify-between items-center gap-2">
-          <CardTitle className="text-lg">{tutor.fullName}</CardTitle>
+          <CardTitle className="text-lg">{tutor?.user?.fullName}</CardTitle>
           <Avatar>
-            <AvatarImage src={tutor.profilePicture} />
+            <AvatarImage src={`https://i.pravatar.cc/150${i}`} />
             <AvatarFallback>
-              {tutor.fullName.slice(0, 2).toUpperCase()}
+              {tutor?.user?.fullName.slice(0, 2).toUpperCase()}
             </AvatarFallback>
           </Avatar>
         </div>
@@ -31,7 +37,7 @@ const TutorCard = ({tutor}) => {
       </CardHeader>
       <CardContent>
         <div className="flex flex-col items-center gap-1">
-          {tutor.expertise.map((it, i) => (
+          {!!tutor?.expertise && tutor.expertise.map((it, i) => (
             <div key={i} className="flex items-center justify-between gap-1">
               <Badge>{it.language}</Badge>
               <Badge variant="outline">{it.experience}+ years</Badge>
@@ -42,12 +48,18 @@ const TutorCard = ({tutor}) => {
       </CardContent>
       <CardFooter className="flex justify-between">
         <p className="text-sm">₹{avgPrice}/hour</p>
-        <Button variant="outline" onClick={() => {console.log("hi");
-        navigate("/tutorProfile");
-        }}>Check</Button>
+        <Button
+          variant="outline"
+          onClick={() => {
+            console.log("hi");
+            navigate("/tutorProfile");
+          }}
+        >
+          Check
+        </Button>
       </CardFooter>
     </Card>
   );
-}
+};
 
-export default TutorCard
+export default TutorCard;
--- Frontend/src/components/tutorList/TutorList.jsx
+++ Frontend/src/components/tutorList/TutorList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 import {
   Pagination,
   PaginationContent,
@@ -8,17 +8,16 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
-import TutorCard from '../tutorCard/TutorCard'
-import { TutorListContext } from '@/pages/HomePage';
+import TutorCard from "../tutorCard/TutorCard";
+import { TutorListContext } from "@/pages/HomePage";
 
 const TutorList = () => {
-  const {tutorList} = useContext(TutorListContext);
+  const { tutorList } = useContext(TutorListContext);
   return (
     <div className="w-full flex flex-col items-center justify-center mb-4">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 place-items-center lg:w-[780px] xl:w-[1000px] my-10">
-        {tutorList.map((tutor, i) => (
-          <TutorCard key={i} tutor={tutor} />
-        ))}
+        {tutorList &&
+          tutorList.map((tutor, i) => <TutorCard key={i} tutor={tutor} i={i} />)}
       </div>
       <Pagination>
         <PaginationContent>
@@ -38,6 +37,6 @@ const TutorList = () => {
       </Pagination>
     </div>
   );
-}
+};
 
-export default TutorList
+export default TutorList;
